948dfb39086700a45a201a82621a1c55464422e4
Alaa Fayed <alaafayed810@gmail.com>
2023-12-17 20:01:14 +0200

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,7 +9,7 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *previous, *temp;
 
-	if (*list == NULL && (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 	{
 		return;
 	}
